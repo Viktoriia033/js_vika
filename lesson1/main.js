c0c0df595b76a112f3f2bb00f4521dd27be66ee6
Viktoriia033 <vtorbych3@gmail.com>
2023-02-01 01:10:35 +0200

    homework1

---
+++ lesson1/main.js
@@ -0,0 +1,40 @@
+// 1
+let aa = 'hello';
+console.log(aa);
+let bb = 'owu';
+console.log(bb);
+let cc = 'com';
+console.log(cc);
+let d = 'ua';
+console.log(d);
+let e = 1;
+console.log(e);
+let f = 10;
+console.log(f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(123);
+let k = 3.14;
+console.log(k);
+let l = 2.7;
+console.log(l);
+let n = 16;
+console.log(n);
+let y = true;
+console.log(y);
+let x = false;
+console.log(x);
+//2
+let lastName = 'Torbych';
+let firstName = 'Viktoriia';
+let middleName = 'Volodymyrivna';
+console.log(lastName + ' ' + firstName + ' ' + middleName);
+//3
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
